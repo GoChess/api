4870c0814c89ae94876993b05afc9b096108df28
Max <sebastian.8295.8295@gmail.com>
2020-10-23 17:46:28 +0100

    initial commit

---
+++ endpoints/ping.go
@@ -0,0 +1,8 @@
+package endpoints
+
+import "net/http"
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Pong"))
+	w.WriteHeader(200)
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", ping())
+	http.ListenAndServe(":3000", r)
+}
